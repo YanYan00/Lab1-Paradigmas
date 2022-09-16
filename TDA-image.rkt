621114c7388024d233bc96d013841f2d4c206334
YanYan00 <jean.rojas@usach.cl>
2022-09-16 02:53:41 -0300

    Cambios en TDAimage

--- TDA-image.rkt
+++ TDA-image.rkt
@@ -1,3 +1,8 @@
 #lang racket
+(require "bitmap.rkt")
 (define (TDAimage alto ancho . datos)
-  datos)
+  (if (and (integer? alto)(integer? ancho)(>= alto 0)(>= ancho 0))
+  (list alto ancho  (car datos))
+  (print "ingresa los datos correctamente")))
+
+(define hola(TDAimage 1 2 '(1 2 3)))
