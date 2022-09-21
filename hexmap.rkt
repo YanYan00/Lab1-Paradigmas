49bc35fb724100ec1cb9db55b07c44a7e005d2d7
Jean Rojas <jean.rojas@usach.cl>
2022-09-21 03:24:51 -0300

    agregar funciones que restringen las creaciones de una imagen si estan ordenadas las coordenadas y si cumple con el tamaño de datos para la imagen, bitmap?,hexmap?,pixmap? creados

--- hexmap.rkt
+++ hexmap.rkt
@@ -1,8 +1,12 @@
 #lang racket
-(define (hexmap x)
-  x)
+(provide (all-defined-out))
 
 (define (pixhex-d x y hex d)
   (if(and(integer? x)(integer? y)(string? hex)(integer? d))
      (list x y hex d)
-   (print "ingresa correctamente los datos")))
+   (print "ingresa correctamente los pixhex")))
+
+(define (hexmap? imagen)
+      (if(and(=(length(caaddr imagen))4)(string? (car(cdr(reverse(car(car(cddr imagen))))))))
+         #t
+         #f))
--- pixmap.rkt
+++ pixmap.rkt
@@ -1,7 +1,13 @@
 #lang racket
-(define (pixmap x)
-  x)
+(provide (all-defined-out))
+
 (define (pixrgb-d x y r g b d)
-  (if(and (integer? x)(integer? y)(integer? r)(integer? g)(integer? b)(integer? d))
+  (if(and (integer? x)(integer? y)(integer? r)(integer? g)(integer? b)(integer? d)
+     (and(>= r 0)(<= r 255))(and(>= g 0)(<= g 255))(and(>= b 0)(<= b 255)))
      (list x y r g b d)
-   (print "Ingrese correctamente los datos")))
+   (print "Ingrese correctamente los pixrgb")))
+
+(define (pixmap? imagen)
+      (if(and(=(length(caaddr imagen))6))
+         #t
+         #f))
