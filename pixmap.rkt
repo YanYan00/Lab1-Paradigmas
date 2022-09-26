688db60849eb0fc40b0c3f939f036c0cd0302cde
Jean Rojas <jean.rojas@usach.cl>
2022-09-26 09:43:02 -0300

    Se agrega la funcions imgrgb->imgHex

--- pixmap.rkt
+++ pixmap.rkt
@@ -1,6 +1,13 @@
 #lang racket
+;--------------Selectores pixrgb-d--------------
 (provide (all-defined-out))
-
+(define (get-x x)
+  (car x))
+(define (get-y x)
+  (cadr x))
+(define (get-d x)
+  (car(reverse x)))
+;--------------TdaPixrgb-------------------------
 (define (pixrgb-d x y r g b d)
   (if(and (integer? x)(integer? y)(integer? r)(integer? g)(integer? b)(integer? d)
      (and(>= r 0)(<= r 255))(and(>= g 0)(<= g 255))(and(>= b 0)(<= b 255)))
